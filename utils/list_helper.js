d1451b92d8213af732730ce6d20e66ec7f9b3b2e
Moritz <22394314+movd@users.noreply.github.com>
2019-10-22 13:35:47 +0200

    Finish Ex. 4.4 unit test sum likes of blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,9 @@
-// Exercises
-
-// Let's create a collection of helper functions that are meant to assist dealing with the blog list. Create the functions into a file called utils/list_helper.js. Write your tests into an appropriately named test file under the tests directory.
-// 4.3: helper functions and unit tests, step1
-
-// First define a dummy function that receives an array of blog posts as a parameter and always returns the value 1. The contents of the list_helper.js file at this point should be the following:
-
 const dummy = () => 1;
 
-module.exports = { dummy };
+const totalLikes = blogs => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
+  }, 0);
+};
 
-// Verify that your test configuration works with the following test:
+module.exports = { dummy, totalLikes };
