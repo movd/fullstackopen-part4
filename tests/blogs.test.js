b5c5053a66daddbe0415734f048babb45e87e993
Moritz <22394314+movd@users.noreply.github.com>
2019-11-04 15:04:24 +0100

    Finish Ex. 4.10 test if a new blog can be added

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -24,8 +24,6 @@ describe("Blog list tests", () => {
 
   test("all blogs are returned", async () => {
     const response = await api.get("/api/blogs");
-    // console.log("response: ", response.body.length);
-    // console.log("should be: ", helper.initialBlogs.length);
     expect(response.body.length).toBe(helper.initialBlogs.length);
   });
 
@@ -36,6 +34,30 @@ describe("Blog list tests", () => {
     expect(response.body[0].id).toBeDefined();
     // Otherwise:
     // response.body.map(blog => expect(blog.id).toBeDefined());
+  });
+
+  test("a new blog can be added", async () => {
+    const newBlog = {
+      title: "SSH Handshake Explained",
+      author: "Russell Jones",
+      url: "https://gravitational.com/blog/ssh-handshake-explained/",
+      likes: 6
+    };
+
+    // POST a newBlog to blogs API
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    // GET from API should now be +1 from initialization
+    const response = await api.get("/api/blogs");
+    expect(response.body.length).toBe(helper.initialBlogs.length + 1);
+
+    // Create array of titles, this should contain 'SSH Handshake Explained'
+    const titles = response.body.map(blog => blog.title);
+    expect(titles).toContainEqual(newBlog.title);
   });
 
   afterAll(() => {
