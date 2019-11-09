adcc6bb6dc00c8124983fcbbfb4e68887258a04a
Moritz <22394314+movd@users.noreply.github.com>
2019-11-09 13:34:14 +0100

    Finish Ex. 4.15 Route for adding users

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -23,5 +24,6 @@ const mongoUrl = config.MONGODB_URI;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
